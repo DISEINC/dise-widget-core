9c29f9313ccb0be32d7eb294f0720b49eb78202d
Emil Johnsen <ej@dise.com>
2017-05-11 14:18:44 +0200

    Uppdatera sökvägar, requirejs till att använda dise-widget-core

--- bootstrap.js
+++ bootstrap.js
@@ -1,7 +1,7 @@
 define(function () {
   "use strict";
 
-  requirejs(["widget", "core/tools", "core/base"], function (Widget, Tools, Base) {
+  requirejs(["widget", "dise-widget-core/tools", "dise-widget-core/base"], function (Widget, Tools, Base) {
     /**
      * By merging all these,
      * the Widget will have access to all public methods on `this`.
