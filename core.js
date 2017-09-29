a9e3f526ae58ab4548d9dfc5576766c45568ba34
Emil Johnsen <ej@dise.com>
2017-09-29 16:19:02 +0200

    Move parameters to DS.Settings

--- core.js
+++ core.js
@@ -23,58 +23,12 @@ define(function () {
     get: function () {
       return new Promise((function (resolve, reject) {
         requirejs(
-          ["text!layouts/" + this.parameters.layout + ".mustache"],
+          ["text!layouts/" + DS.Settings.layout + ".mustache"],
           (function (layout) {
             resolve(layout);
           }).bind(this)
         );
       }).bind(this));
-    }
-  });
-
-
-  Object.defineProperty(api, 'parameters', {
-    enumerable: true,
-    get: function () {
-      var removeEmpty = function (p) {
-        return p[1] != undefined && p[1] != ""
-      };
-      var toObject = function (m, n) {
-        m[n[0]] = n[1];
-        return m;
-      };
-
-      /**
-       * Retrieve parameters from the URL
-       */
-      var parameters =  location.search.replace(/^\?/, "").split("&")
-        .map(function (p) { return p.split("="); })
-        .filter(removeEmpty)
-        .reduce(toObject, {});
-
-      /**
-       * And default parameters from the body.
-       */
-      var defaults = Array.prototype.slice.call(document.querySelectorAll('meta[name^="widget-"]'))
-        .map(function (tag) { return [tag.name.replace("widget-", ""), tag.content.trim()]; })
-        .filter(removeEmpty)
-        .reduce(toObject, {});
-
-      /**
-       * Essentially a copy of Tools.merge.
-       */
-      var merged = Object.create({});
-      [defaults, parameters]
-      .map(function (obj) {
-        Object.keys(obj).forEach(function (key) {
-          if (obj.hasOwnProperty(key))
-          {
-            merged[key] = obj[key];
-          }
-        });
-      });
-
-      return merged;
     }
   });
 
@@ -101,10 +55,10 @@ define(function () {
 
     this.emit('render');
 
-    if (this.parameters.tick > 0) {
+    if (DS.Settings.tick > 0) {
       window.setInterval(
         (function () { this.emit('tick'); }).bind(this),
-        this.parameters.tick * 1000
+        DS.Settings.tick * 1000
       );
     }
 
--- index.js
+++ index.js
@@ -5,8 +5,9 @@ define(function () {
     "widget",
     "dise-widget-core/tool",
     "dise-widget-core/async",
-    "dise-widget-core/core"
-  ], function (Widget, Tool, Async, Core) {
+    "dise-widget-core/core",
+    "dise-widget-core/settings",
+  ], function (Widget, Tool, Async, Core, Settings) {
     /**
      * By merging all these,
      * the Widget will have access to all public methods on `this`.
@@ -18,7 +19,8 @@ define(function () {
       Widget: Widget,
       Tool: Tool,
       Async: Async,
-      Core: Core
+      Core: Core,
+      Settings: Settings
     };
 
     DS.Core.setup();
