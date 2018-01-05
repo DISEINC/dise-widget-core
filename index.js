2ce5bbe29beb9ef0dbba7b51e550d8c2edb40e15
Emil Johnsen <emil.johnsen@dise.com>
2018-01-05 20:29:29 +0100

    Rename DS.Tool to DS.util

--- index.js
+++ index.js
@@ -3,16 +3,16 @@ define(function () {
 
   requirejs([
     "widget",
-    "dise-widget-core/lib/tool",
+    "dise-widget-core/lib/util",
     "dise-widget-core/lib/async",
     "dise-widget-core/lib/core",
     "dise-widget-core/lib/settings",
     "dise-widget-core/lib/cache",
-  ], function (Widget, Tool, Async, Core, Settings, Cache) {
+  ], function (Widget, Util, Async, Core, Settings, Cache) {
 
     window.DS = {
       Widget: Widget,
-      Tool: Tool,
+      Util: Util,
       Async: Async,
       Core: Core,
       Settings: Settings,
